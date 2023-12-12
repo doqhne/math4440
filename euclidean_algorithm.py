a8672eeec93ac4b28e24424fe7ff036cf602361d
Daphne <daqu2831@colorado.edu>
2023-12-12 23:53:35 +0000

    euclidean algorithm code

---
+++ euclidean_algorithm.py
@@ -0,0 +1,26 @@
+'''Calculate the gcd of 2 numbers'''
+num1 = int(input('Please enter the first number for the gcd: '))
+num2 = int(input('Please enter the second number for the gcd: '))
+
+# make sure a is larger than b
+a = max(num1, num2)
+b = min(num1, num2)
+
+def get_gcd(a, b):
+    # gcd(0, n) = n
+    if b == 0:
+        return a
+    
+    # continue finding residues until we get zero
+    while b != 0:
+        residue = a%b
+        # success, return the gcd
+        if residue == 0:
+            return b 
+        # keep going
+        else:
+            # shift a and b
+            a = b
+            b = residue
+            
+print(f'gcd({num1}, {num2}) = {get_gcd(a, b)}')
